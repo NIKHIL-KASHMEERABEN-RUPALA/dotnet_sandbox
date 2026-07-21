ee4faeb10d9892bcbdf35670d29d55a66b8e405b
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-21 07:50:22 +0530

    some changes

--- GMS_project/GrievanceManagementSystem/GrievanceManagementSystem/obj/Debug/net10.0/GrievanceManagementSystem.AssemblyInfo.cs
+++ GMS_project/GrievanceManagementSystem/GrievanceManagementSystem/obj/Debug/net10.0/GrievanceManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrievanceManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e75de2d6c923103afc5d74e325b5476746d7736")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrievanceManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrievanceManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
