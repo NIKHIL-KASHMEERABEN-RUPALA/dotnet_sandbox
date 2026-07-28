a4a4620045a203e8a82147737ac5023980cb0b47
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:50:09 +0000

    completed with 66.cs

--- leet_code_ques/66.cs
+++ leet_code_ques/66.cs
@@ -12,10 +12,17 @@ public class Solution
 
                 digits[i] = digits[i]+1;
                 return digits;
-                
+
             }
             digits[i] = 0;
             // if we encounter 9 then initialize it to zero
         }
+        // if we made it till the end of for loop , it means all elements are 9 
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+
+        // all the array element by default are zero and 0th index we intialized to 1
+
+        return arr;
     }
 }
