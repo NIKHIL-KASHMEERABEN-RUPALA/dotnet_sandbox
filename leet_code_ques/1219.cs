bf69e93cf677568232e1b954540b90b1c03bc019
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:25:29 +0000

    made a logic for bool visited array and initialized max array

--- leet_code_ques/1219.cs
+++ leet_code_ques/1219.cs
@@ -12,8 +12,19 @@ public class Solution{
 
     int m = grid.Length; // represents total number of rows
     int n = grid.Length[0]; // represents total number of columns
+    int max = 0;
+    bool[][] visited = new bool[m][];
+     for (int k = 0; k < m; k++) {
+        visited[k] = new bool[n];
+    }
 
-    
+    for(int i = 0 ; i < m ; i++)
+        {
+            for(int j = 0 ; j<n ; j++)
+            {
+                
+            }
+        }
 
     }
 
