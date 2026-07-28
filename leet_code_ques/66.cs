0977459ca4c770aa82b22b1c200d3fd31a41a61d
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:48:50 +0000

    next if we encounter a 9 , then we make that array box equals to 0

--- leet_code_ques/66.cs
+++ leet_code_ques/66.cs
@@ -9,9 +9,10 @@ public class Solution
 
             if (digits[i] < 9)
             {
-                
+
                 digits[i] = digits[i]+1;
                 return digits;
+                
             }
             digits[i] = 0;
             // if we encounter 9 then initialize it to zero
