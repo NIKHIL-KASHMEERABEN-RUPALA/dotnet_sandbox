d70a814fbd4d8a865f4d08645927c36d855da16f
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:47:05 +0000

    made a for loop

--- leet_code_ques/66.cs
+++ leet_code_ques/66.cs
@@ -4,5 +4,15 @@ public class Solution
     {
         int n = digits.Length;
         // here we extract length of digits array
+        for(int i = n-1 ; i>=0 ; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i] = digits[i]+1;
+                return digits;
+            }
+            digits[i] = 0;
+            // if we encounter 9 then initialize it to zero
+        }
     }
 }
