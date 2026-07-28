88a61ae770533b9e286e90028d8a94885c1635d9
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:48:21 +0000

    if the digit is less than 9 then we simply add plus one and return full digit array

--- leet_code_ques/66.cs
+++ leet_code_ques/66.cs
@@ -6,9 +6,10 @@ public class Solution
         // here we extract length of digits array
         for(int i = n-1 ; i>=0 ; i--)
         {
-            
+
             if (digits[i] < 9)
             {
+                
                 digits[i] = digits[i]+1;
                 return digits;
             }
