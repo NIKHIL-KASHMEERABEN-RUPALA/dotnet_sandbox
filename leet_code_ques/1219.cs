c8d63f325fcf43422d7a7098f6addd194be57a35
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-28 14:22:33 +0000

    initialized all the required logic and boiler code

--- leet_code_ques/1219.cs
+++ leet_code_ques/1219.cs
@@ -1,3 +1,15 @@
 
 // Path with Maximum Gold
 
+public class Solution{
+
+
+    public int GetMaximumGold(int[][] grid){
+        if (grid==null || grid.Length==0){
+            return 0;
+            // coz if grid 2-d array is empty or grid 2d array has no elements then we return 0
+        }
+    }
+
+
+}
