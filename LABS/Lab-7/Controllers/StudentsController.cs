f20841528606998ffd6810aa36f8647263b77dc6
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-07-29 07:06:17 +0000

    made certain changes in program.cs

---
+++ LABS/Lab-7/Controllers/StudentsController.cs
@@ -0,0 +1,10 @@
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mcs;
+using StudentManagementApi.Models;
+
+namespace StudentManagementApi.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    
+}
